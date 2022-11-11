e2aa8d550487704e15883d8cfbe6c647c2ab38ea
amochkin <amochkin@gmail.com>
2022-11-11 12:50:33 -0500

    Added ESM to build output and types

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { setValueByPath } from './jsonValueByPath';
+import { getValueByPath } from './jsonValueByPath';
+import { castValueToType, stringToValidTypeString } from './castValueToType';
+
+export { setValueByPath, getValueByPath, castValueToType, stringToValidTypeString };
